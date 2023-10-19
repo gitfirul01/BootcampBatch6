c7debef944f4c7a20a9e19fcf43a0a5911bf241d
gitfirul01 <46it.widiyanto@gmail.com>
2023-10-19 14:02:09 +0700

    day7

---
+++ Day07_04_GetInvocationList__return_string/Program.cs
@@ -0,0 +1,23 @@
+﻿//GetInvocationList (return string)
+public delegate string MyDelegate();
+void Main()
+{
+	MyDelegate mydel = Message;
+	mydel += PrintOut;
+	
+	List<string> result = new();
+	Delegate[] delegateList = mydel.GetInvocationList();
+	foreach(MyDelegate d in delegateList) {
+		result.Add(d.Invoke());
+	}
+	result.Dump();
+}
+
+string Message() {
+	return "message";
+}
+string PrintOut() {
+	return "printout";
+} 
+
+// You can define other methods, fields, classes and namespaces here
---
+++ Day07_05_add_delegate_member/Program.cs
@@ -0,0 +1,22 @@
+﻿//+=
+public delegate string MyDelegate();
+void Main()
+{
+	MyDelegate mydel = Message;
+	MyDelegate mydel2 = PrintOut;
+	MyDelegate result = mydel + mydel2;
+
+	MyDelegate mydelll = Message;
+	mydelll += PrintOut;
+}
+
+string Message()
+{
+	return "message";
+}
+string PrintOut()
+{
+	return "printout";
+}
+
+// You can define other methods, fields, classes and namespaces here
