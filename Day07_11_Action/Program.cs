b7fb09b08af5c9f0dde6e616186f0a357e53058d
gitfirul01 <46it.widiyanto@gmail.com>
2023-10-19 20:48:33 +0700

    Day7

---
+++ Day07_11_Action/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Action
+    public delegate void Action<in T>(T obj)
+*/
+
+class Program {
+    static void Main() {
+        
+    }
+}
+
+class CardCreator {
+	public List<Card> cards;
+	public Action<Card> onCardUpdate;
+	public void CreateCard(int id, string description) {
+		Card card = new Card() {id = id, description = description};
+		cards.Add(card);
+		Update(card);
+	}
+	public void Update(Card card) {
+		onCardUpdate?.Invoke(card);
+	}
+}
+
+class Card { 
+	public int id;
+	public string description;
+}
+
+class UI {
+	public void Notification(Card data) {
+		Console.WriteLine($"Update UI location {data.id} , {data.description}");
+	}
+}
+
+class Database {
+	public void AddCardToDb(Card data) {
+		Console.WriteLine($"Update Database {data.id} , {data.description}");
+	}
+}
