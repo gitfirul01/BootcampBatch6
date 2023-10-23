1862b14840b1ac004f6a436a6f852006a47d7bf2
gitfirul01 <46it.widiyanto@gmail.com>
2023-10-23 21:10:55 +0700

    fixing Tugas01_Foo_Bar

--- Day09/Day09_03_Enumerator/Program.cs
+++ Day09/Day09_03_Enumerator/Program.cs
@@ -1,17 +1,27 @@
-﻿//Enumerator
+﻿// Enumerator
 class Program {
     static void Main() {	
+		//
     }
 }
 
-class MyCollection : IEnumerator
+class MyCollection : IEnumerator<int>
 {
 	public int currentIndex = -1;
 	public int[] myInt = new int[5];
 	
 	public object Current => myInt[currentIndex];
 
-	public bool MoveNext()
+	// BEGIN: implementation
+    int IEnumerator<int>.Current => throw new NotImplementedException();
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+	// END: impementation
+
+    public bool MoveNext()
 	{
 		if(currentIndex+1 < myInt.Length) {
 			currentIndex++;
--- _Tugas/Tugas01_Foo_Bar/Program.cs
+++ _Tugas/Tugas01_Foo_Bar/Program.cs
@@ -1,11 +1,19 @@
-﻿class Program {
+﻿// Program.cs
+class Program {
     static void Main(){
-        int n;
+        int n = 15;
+        string result;
+
+        result = FooBar.Run(n);
+        Console.WriteLine(result);
+    }
+}
+
+
+// FooBar.cs
+static class FooBar {
+    public static string Run (int n) {
         string output = "";
-
-        Console.Write("n: ");
-        n = Int32.Parse(Console.ReadLine());
-
         for (int i=0; i<=n; i++) {
             if ((i == 0) || ((i%3 != 0) && (i%5 != 0))) {
                 output += i.ToString();
@@ -18,11 +26,10 @@
                     output += "Bar";
                 }
             }
-
             if (i != n) {
                 output += ", ";
             }
         }
-        Console.WriteLine(output);
-    }
+        return output;
+    } 
 }
